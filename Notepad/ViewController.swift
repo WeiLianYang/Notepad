693533e8d2c9f5d20f660c2efe0974d4ab52c085
William <yangchen_0808@163.com>
2022-02-10 22:19:25 +0800

    integrate SnapKit

--- Notepad/ViewController.swift
+++ Notepad/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         self.title = "Notepad"
+        self.navigationController?.navigationBar.backgroundColor = UIColor.orange
     }
 
 }
---
+++ Pods/Target Support Files/Pods-Notepad/Pods-Notepad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Notepad : NSObject
+@end
+@implementation PodsDummy_Pods_Notepad
+@end
