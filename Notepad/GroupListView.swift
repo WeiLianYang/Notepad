ecea6c3f5e45404f56908a0c7bb66526b8b51fa4
William <yangchen_0808@163.com>
2022-02-12 22:01:25 +0800

    add Notepad group view

---
+++ Notepad/GroupListView.swift
@@ -0,0 +1,65 @@
+//
+//  GroupListView.swift
+//  Notepad
+//
+//  Created by WilliamYang on 2022/2/12.
+//
+
+import UIKit
+
+class GroupListView: UIScrollView {
+
+    // 列间距
+    let interitemSpacing = 15
+    
+    // 行间距
+    let lineSpacing = 20
+    
+    // 分组标题数组
+    var dataArray: Array<String>?
+    
+    // 分组按钮数组
+    var buttonArray: Array<UIButton> = Array<UIButton>()
+    
+    func updateLayout() {
+        // 计算每个按钮的宽高
+        let itemWidth = (self.frame.size.width - CGFloat(4 * interitemSpacing)) / 3
+        let itemHeight = itemWidth
+        
+        // 移除已有的按钮
+        buttonArray.forEach({ element in
+            element.removeFromSuperview()
+        })
+        // 清空所有按钮
+        buttonArray.removeAll()
+        
+        // 开始布局
+        if dataArray != nil && dataArray!.count > 0 {
+            // 遍历
+            for index in 0 ..< dataArray!.count {
+                let btn = UIButton(type: .system)
+                btn.setTitle(dataArray![index], for: .normal)
+                // 计算按钮位置
+                btn.frame = CGRect(x: CGFloat(interitemSpacing) + CGFloat(index % 3) * (itemWidth + CGFloat(interitemSpacing)), y: CGFloat(lineSpacing) + CGFloat(index / 3) * (itemHeight + CGFloat(lineSpacing)), width: itemWidth, height: itemHeight)
+                
+                btn.backgroundColor = UIColor(red: 1, green: 245 / 255.0, blue: 215 / 255.0, alpha: 1)
+                
+                // 设置圆角
+                btn.layer.masksToBounds = true
+                btn.layer.cornerRadius = 16
+                btn.setTitleColor(UIColor.gray, for: .normal)
+                btn.tag = index
+                btn.addTarget(self, action: #selector(onBtnClick), for: .touchUpInside)
+                self.addSubview(btn)
+                buttonArray.append(btn)
+            }
+            // 设置滚动视图内容尺寸
+            self.contentSize = CGSize(width: 0, height: buttonArray.last!.frame.origin.y + buttonArray.last!.frame.size.height + CGFloat(lineSpacing))
+        }
+    }
+    
+    @objc func onBtnClick(btn: UIButton) {
+        print(dataArray![btn.tag])
+    }
+
+}
--- Notepad/ViewController.swift
+++ Notepad/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var groupView: GroupListView?
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -19,6 +21,19 @@ class ViewController: UIViewController {
         
         self.title = "Notepad"
         self.navigationController?.navigationBar.backgroundColor = UIColor.orange
+        
+        groupView = GroupListView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height - 30))
+        
+        self.view.addSubview(groupView!)
+        
+//        groupView?.dataArray = ["生活", "工作", "逛街", "购物", "健身", "旅行", "聚会", "探亲", "访友", "学习", "比赛", "会议", "考试"]
+        
+        var groupArray: Array<String> = []
+        for index in 0 ... 24 {
+            groupArray.append("Group \(index)")
+        }
+        groupView?.dataArray = groupArray
+        groupView?.updateLayout()
     }
 
 }
